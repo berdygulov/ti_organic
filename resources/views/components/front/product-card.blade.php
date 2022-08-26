f3601d8377ec1e67ff67deb06bbed5fc55ba1002
berdygulov <berdygulov1997@mail.ru>
2022-08-26 19:22:55 +0500

    product card component is in process 0.2

--- resources/views/components/front/product-card.blade.php
+++ resources/views/components/front/product-card.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="product_card rounded p-3.9 w-[300px] shadow">
+    <div class="product_card rounded p-3.9 w-[300px]">
         <div class="product-top mb-3.9">
             <span class="discount inline-block px-[7px] py-[5px] bg-green text-white text-sm rounded">
                 -50%
@@ -9,21 +9,21 @@
             <img src="{{ asset('assets/images/png/product-card-image.png') }}" alt="product-image">
         </div>
         <div class="product-content">
-            <div class="product-title mb-[3px] font-bold">
+            <div class="product-title text-lg mb-[3px] font-bold">
                 Увлажняющая маска Yves Rocher
             </div>
-            <span class="product-cat text-xs text-gray-2 mb-2.5 lowercase">
+            <span class="block product-cat text-sm text-gray-2 mb-2.5 lowercase">
                 уход за лицом / маски
             </span>
-            <div class="mb-3.9 text-gray-3 lowercase text-xs">
-                <span class="product-code">
+            <div class="mb-3.9 text-gray-3 lowercase text-sm">
+                <span class="product-code block mb-[3px]">
                     code: 1521892
                 </span>
-                <span class="product-quantity">
+                <span class="product-quantity block">
                     количество: 5 шт.
                 </span>
             </div>
-            <div class="product-price">
+            <div class="product-price font-bold text-lg">
                 12 000 ₸
             </div>
         </div>
